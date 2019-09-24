bd11fb0cdc49287679b18bb38a0414ff5af631ec
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-09-24 16:30:45 -0700

    Added axios package that holds in as a value of a variable called axios by using require

--- liri.js
+++ liri.js
@@ -7,3 +7,18 @@ var keys = require("./keys.js");
 //Access the keys information with spotify
 var spotify = new Spotify(keys.spotify);
 
+/*
+Make liri.js take these commands in:
+conert-this
+spotify-this-song
+movie-this
+do-what-it-says
+*/
+
+//BandsinTown Concerts
+/* Name of the venue
+Venue location
+Date of the Event (use moment to format this as "MM/DD/YYYY")*/
+
+//Include axios npm package
+var axios = require("axios");
