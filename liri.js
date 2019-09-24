d4672183995377cdb471da972086424b91f8191d
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-09-24 16:23:48 -0700

    Accessed the keys information with spotify by making a new variable called spotify and a value of new Spotify()

--- liri.js
+++ liri.js
@@ -4,3 +4,6 @@ require('dotenv').config();
 //A variable that imports the keys.js file
 var keys = require("./keys.js"); 
 
+//Access the keys information with spotify
+var spotify = new Spotify(keys.spotify);
+
