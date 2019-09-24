09c2e151e744de6a6e534613e55c3ac0f8caafbb
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-09-24 16:46:25 -0700

    installed dotenv

--- liri.js
+++ liri.js
@@ -1,5 +1,5 @@
 //Reads and sets any evironment variables with the 'dotenv' package
-require('dotenv').config();
+require("dotenv").config();
 
 //A variable that imports the keys.js file
 var keys = require("./keys.js"); 
@@ -15,7 +15,7 @@ movie-this
 do-what-it-says
 */
 
-//BandsinTown Concerts
+//============================= [BandsinTown Concerts] =============================
 /* Name of the venue
 Venue location
 Date of the Event (use moment to format this as "MM/DD/YYYY")*/
@@ -28,3 +28,6 @@ var artistName = process.argv[2];
 
 //Make a url request with BandsinTown
 var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+//Log the URL for future de-bugging.
+console.log(queryURL);
