0d5fa5f0cf00d88bae725c0b3e5cc8a39c1f4101
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-09-25 14:47:32 -0700

    Added the plot of movie

--- liri.js
+++ liri.js
@@ -114,7 +114,7 @@ axios.get(movieURL).then(
         //This logs the Country where the movie was produced
         console.log("\nYear: " + response.Country + "\n");
         //This logs the Plot of the movie
-
+        console.log("\nYear: " + response.Plot + "\n");
         //This logs the Actors in the movie
     }
 )
